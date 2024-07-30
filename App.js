4dab681f038e714e483d8bca53684cc6eb3f3670
Jay Vince Serato <jayvince.serato@gmail.com>
2024-07-30 13:55:03 +0800

    Add basic styles

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={{margin:50, borderWidth:2, borderColor:'red', padding:16}}>Whttaops</Text>
+      </View>
+      
+      <Text>Team ALAGAD</Text>
       <StatusBar style="auto" />
+      <View style={styles.bot}>
+        <Button title='Tap Me'/>
+      </View>
     </View>
+     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#cf7c20',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bot: {
+    flex: 1,
+    backgroundColor: '#5f7420',
+    alignSelf: 'baseline',
+    alignContent:'space-evenly'
   },
 });
